2931f5dbfebece1f8deea294998adf0f3b1c8500
Yannick Stade <100073938+ystade@users.noreply.github.com>
2026-01-28 10:56:42 +0100

    🎨 Improve docstrings

--- include/na/zoned/decomposer/NativeGateDecomposer.hpp
+++ include/na/zoned/decomposer/NativeGateDecomposer.hpp
@@ -19,10 +19,19 @@
 namespace na::zoned {
 
 class NativeGateDecomposer : public DecomposerBase {
+  /**
+   * A minimal struct to store the parameters of a U3 gate along with the qubit
+   * it acts on.
+   */
   struct StructU3 {
     std::array<qc::fp, 3> angles;
     qc::Qubit qubit;
   };
+  /**
+   * A quaternion is represented by an array of four `qc::fp` values `{q0, q1,
+   * q2, q3}` denoting the components of the quaternion.
+   */
+  using Quaternion = std::array<qc::fp, 4>;
   size_t nQubits_ = 0;
 
   constexpr static qc::fp epsilon =
@@ -51,61 +60,61 @@ public:
    * @code quaternion(R_v(phi)) = (cos(phi/2) * I, v0 * sin(phi/2) * X, v1 *
    * sin(phi/2) * Y, v2 * sin(phi/2) * Z)@endcode with X, Y, Z Pauli Matrices.
    * @param op a reference_wrapper to the operation to be converted
-   * @returns an array of four `qc::fp` values `{q0, q1, q2, q3}` denoting the
-   * components of the quaternion.
+   * @returns a quaternion.
    */
   static auto
   convert_gate_to_quaternion(std::reference_wrapper<const qc::Operation> op)
-      -> std::array<qc::fp, 4>;
+      -> Quaternion;
   /**
-   * Merges the quaternions representing two gates as in a Matrix multiplication
-   * of the gates
-   * @param q1 the first Quaternion to be combined
-   * @param q2 the second Quaternion to be combined
-   * @returns an array of four qc::fp values [q0,q1,q2,q3] denoting the
-   * components of the combined quaternion
+   * @brief Merges the quaternions representing two gates as in a matrix
+   * multiplication of the gates.
+   * @param q1 the first quaternion to be combined.
+   * @param q2 the second quaternion to be combined.
+   * @returns an quaternion.
    */
-  static auto combine_quaternions(const std::array<qc::fp, 4>& q1,
-                                  const std::array<qc::fp, 4>& q2)
-      -> std::array<qc::fp, 4>;
+  static auto combine_quaternions(const Quaternion& q1, const Quaternion& q2)
+      -> Quaternion;
   /**
-   * Calculates the values of the U3-Gate parameters theta, phi and lambda
-   * @param quat is a quaternion representing a single qubit gate
-   * @returns an array of three qc::fp values [theta, phi, lambda] giving the U§
-   * gate angles
+   * @brief Calculates the values of the U3-gate parameters theta, phi, and
+   * lambda.
+   * @param quat is a quaternion representing a single qubit gate.
+   * @returns an array of three `qc::fp` values `{theta, phi, lambda}` giving
+   * the U3 gate angles.
    */
-  static auto get_U3_angles_from_quaternion(const std::array<qc::fp, 4>& quat)
+  static auto get_U3_angles_from_quaternion(const Quaternion& quat)
       -> std::array<qc::fp, 3>;
 
   /**
-   * Calculates the largest value of the U3-Gate parameter theta from a vector
-   * of Operations. Fails when provided gates aren't all U3-Gates.
-   * @param layer is a SingleQubitGateLayers of a scheduled
-   * @returns the maximal value of theta in the given layer
+   * @brief Calculates the largest value of the U3-gate parameter theta from a
+   * vector of operations.
+   * @param layer is a vector of U3 parameters.
+   * @returns the maximal value of theta in the given layer.
    */
   static auto calc_theta_max(const std::vector<StructU3>& layer) -> qc::fp;
 
   /**
-   * Takes a vector of SingleQubitGateLayer's and, for each layer
-   * , transforms all gates into U3 gates represented by struct_U3's
-   * , combining  all gates acting on the same qubit into a single U3 gate
+   * @brief Takes a vector of SingleQubitGateLayers and, for each layer,
+   * transforms all gates into U3 gates represented by `StructU3` objects.
+   * @details It combines all gates acting on the same qubit into a single U3
+   * gate.
    * @param layers is a std::vector of SingleQubitGateLayers of a scheduled
-   * circuit
-   * @returns a vector of vectors of a struct_U3's representing the single qubit
-   * gate layers
+   * circuit.
+   * @returns a vector of vectors of StructU3 objects representing the single
+   * qubit gate layers.
    */
   [[nodiscard]] auto
   transform_to_U3(const std::vector<SingleQubitGateRefLayer>& layers) const
       -> std::vector<std::vector<StructU3>>;
   /**
-   * Takes a vector of qc::fp's representing the U3-Gate angles of a single
-   * qubit hate and the maximal value of theta for the single qubit gate layer
-   * and calculates the transversal decomposition angles as in Nottingham et.
-   * al. 2024
-   * @param angles is a std::array of qc::fp representing (theta,phi, lambda)
-   * @param theta_max the maximal theta value of the single-qubit qate layer
-   * @returns an array of qc::fp values giving the angles (chi, gamma_minus,
-   * gamma_plus)
+   * @brief Takes a vector of `qc::fp` representing the U3-gate angles of a
+   * single-qubit gate and the maximal value of theta for the single qubit gate
+   * layer and calculates the transversal decomposition angles as in Nottingham
+   * et. al. 2024.
+   * @param angles is a `std::array` of `qc::fp` representing (theta, phi,
+   * lambda).
+   * @param theta_max the maximal theta value of the single-qubit qate layer.
+   * @returns an array of `qc::fp` values giving the angles (chi, gamma_minus,
+   * gamma_plus).
    */
   auto static get_decomposition_angles(const std::array<qc::fp, 3>& angles,
                                        qc::fp theta_max)
